3abf93267cb2619e51a2a8d4590afb7a6f6b5600
Hadeed <hadeedji@gmail.com>
2021-02-24 00:12:40 +0500

    Refactor SnakePiece.cs

--- Snake/src/SnakePiece.cs
+++ Snake/src/SnakePiece.cs
@@ -7,21 +7,25 @@ public class SnakePiece {
     public SnakePiece(int x, int y) {
         this.x = x;
         this.y = y;
+
+
+        xInPixels = lineWidth + x * (lineWidth + cellSize);
+        yInPixels = lineWidth + y * (lineWidth + cellSize);
+
+        rectangle = new Rectangle(xInPixels, yInPixels, cellSize, cellSize);
     }
 
     public int x { get; }
     public int y { get; }
-
-    public Rectangle rectangle => new Rectangle(xInPixels, yInPixels, cellSize, cellSize);
+    public Rectangle rectangle { get; }
 
     public Direction outDirection { get; set; }
 
+    private int xInPixels { get; }
+    private int yInPixels { get; }
+
     private int lineWidth => configuration.dimensions.line;
     private int cellSize => configuration.dimensions.cell;
-
-    private int xInPixels => lineWidth + x * (lineWidth + cellSize);
-    private int yInPixels => lineWidth + y * (lineWidth + cellSize);
-
 
     public Rectangle Offset(Direction offsetDirection, int pixels) =>
         offsetDirection switch {
@@ -37,7 +41,7 @@ public class SnakePiece {
             Direction.Right => new Rectangle(xInPixels + pixels, yInPixels,
                                              lineWidth + cellSize - pixels, cellSize),
 
-            _ => throw new ArgumentOutOfRangeException(nameof(offsetDirection), offsetDirection, null)
+            _ => throw new ArgumentOutOfRangeException()
         };
 
     public Rectangle LineBetween(SnakePiece piece) {
@@ -47,25 +51,8 @@ public class SnakePiece {
             (-1, 0) => new Rectangle(xInPixels - lineWidth, yInPixels, lineWidth, cellSize),
             (0, 1) => new Rectangle(xInPixels, yInPixels + cellSize, cellSize, lineWidth),
             (0, -1) => new Rectangle(xInPixels, yInPixels - lineWidth, cellSize, lineWidth),
-            _ => new Rectangle(0, 0, 0, 0)
+            _ => throw new ArgumentOutOfRangeException()
         };
-    }
-
-    protected bool Equals(SnakePiece other) {
-        return x == other.x && y == other.y;
-    }
-
-    public override bool Equals(object obj) {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((SnakePiece) obj);
-    }
-
-    public override int GetHashCode() {
-        unchecked {
-            return (x * 397) ^ y;
-        }
     }
 
     public static bool operator ==(SnakePiece left, SnakePiece right) {
@@ -82,6 +69,23 @@ public class SnakePiece {
 
     public static (int x, int y) operator -(SnakePiece piece1, SnakePiece piece2) {
         return (piece1.x - piece2.x, piece1.y - piece2.y);
+    }
+
+    public override int GetHashCode() {
+        unchecked {
+            return (x * 397) ^ y;
+        }
+    }
+
+    public override bool Equals(object obj) {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((SnakePiece) obj);
+    }
+
+    protected bool Equals(SnakePiece other) {
+        return x == other.x && y == other.y;
     }
 }
 }
